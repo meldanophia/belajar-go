714e625e1fc118f33236d428572397083156edb6
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-04-30 11:03:11 +0700

    declare a variable

---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Princess"
+
+	fmt.Println(name)
+
+	lili := "lalalla"
+
+	fmt.Println(lili)
+
+	var (
+		firstName = "Melda"
+		lastName  = "Nophia"
+	)
+
+	fmt.Println(firstName + " " + lastName)
+}
