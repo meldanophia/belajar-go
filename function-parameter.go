d3e629feb944c4086cd059d59bbf83d2c340ddf9
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-05-03 16:05:41 +0700

    how parameter does in function

---
+++ function-parameter.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func sayHello(firstName string, lastName string) {
+	fmt.Println("hai " + firstName + " " + lastName)
+}
+
+func main() {
+	sayHello("Melda", "Nophia")
+}
