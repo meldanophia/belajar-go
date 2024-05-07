6256358fc6700408ec52d9d0edc720726d866c5a
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-05-07 15:25:15 +0700

    function as parameter

---
+++ function-as-parameter.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func sayHelloWithFilter(name string, filter func(string) string) {
+	fmt.Println("Hello " + filter(name))
+}
+
+func spamFilter(name string) string {
+	if name == "anjing" {
+		return "..."
+	} else {
+		return name
+	}
+}
+
+func main() {
+	sayHelloWithFilter("Melda", spamFilter)
+
+	filter := spamFilter
+	sayHelloWithFilter("anjing", filter)
+}
