4e83e991da2766c5e5d4f0428ce15bb24614f8c5
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-05-13 15:33:55 +0700

    struct literals
    ad byk cara yg bs digunakan utk membuat data dri struct

--- struct.go
+++ struct.go
@@ -14,4 +14,14 @@ func main() {
 	melda.Age = 25
 
 	fmt.Println(melda)
+
+	joko := Customer{
+		Name:    "Joko",
+		Address: "Jakarta",
+		Age:     30,
+	}
+	fmt.Println(joko)
+
+	budi := Customer{"Budi", "Semarang", 35}
+	fmt.Println(budi)
 }
