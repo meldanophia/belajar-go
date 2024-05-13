e8b8d7c716093109ef0df9263cf2ab9ccd2ccd2d
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-05-13 16:12:24 +0700

    implementasi interface 2
    apabila structnya > 1 bukan masalah.

--- interface.go
+++ interface.go
@@ -18,8 +18,20 @@ func (person Person) getName() string {
 	return person.Name
 }
 
+// struct animal mengimplementasikan interface hasName
+// karena memiliki funcion getName yg sesuai dgn interface hasName
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) getName() string {
+	return animal.Name
+}
+
 func main() {
 	person := Person{Name: "Melda"}
-
 	sayHi(person)
+
+	animal := Animal{Name: "Hachi"}
+	sayHi(animal)
 }
