d6332c5d1b9fc1a13f911352cd4b11ca222ad7c1
ENSEVAL\melda.nophia <meldanophia@gmail.com>
2024-05-13 15:31:54 +0700

    data struct
    struct tdk bs lgsg digunakan, kita bs membuat object/data dri struct yg tlh kita buat

--- struct.go
+++ struct.go
@@ -1,10 +1,17 @@
 package main
 
+import "fmt"
+
 type Customer struct {
 	Name, Address string
 	Age           int
 }
 
 func main() {
+	var melda Customer
+	melda.Name = "Melda Nophia"
+	melda.Address = "Bekasi"
+	melda.Age = 25
 
+	fmt.Println(melda)
 }
