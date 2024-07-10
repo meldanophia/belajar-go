1ec16ef008a638d1637f9cb3dfca71615a4fa5bc
meldanophia <meldanophia@gmail.com>
2024-07-10 15:11:15 +0700

    type assertions
    kemampuan merubah tipe data mjd tipe data yg diinginkan
    
    fitur ini sering digunakan ketika bertemu dgn data interface kosong

---
+++ type_assertions.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func random() any {
+	return "OK"
+}
+
+func main() {
+	var result any = random()
+	fmt.Println(result) // OK
+
+	// implementing type assertions using .(data-type)
+	resultString := result.(string)
+	fmt.Println(resultString) // OK
+
+	// invalid operation: resultString (variable of type string) is not an interface
+	// resultInt := resultString.(int) // panic
+	// fmt.Println(resultInt)
+}
