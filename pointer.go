ee375f57c8a6475369c900c527fca1b807248f02
meldanophia <meldanophia@gmail.com>
2024-07-10 15:56:12 +0700

    pointer
    pointer adlh kemampuan membuat reference ke lokasi data di memory yg sama, tanpa menduplikasi data yg sudah ada
    
    kesimpulannya, dgn pointer, kita bs membuat pass by reference
    
    utk membuat sebuah var dgn nilai pointer ke var yg lain, bs menggunakan operator & diikuti dgn nama variablenya

--- pointer.go
+++ pointer.go
@@ -7,11 +7,20 @@ type Address struct {
 }
 
 func main() {
-	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
-	address2 := address1
+	// address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+
+	// address2 := address1
+	// address2.City = "Bandung"
+
+	// fmt.Println(address1) // {Subang Jawa Barat Indonesia}
+	// fmt.Println(address2) // {Bandung Jawa Barat Indonesia}
+
+	var address1 Address = Address{"Subang", "Jawa Barat", "Indonesia"}
+	var address2 *Address = &address1 // pointer
 
 	address2.City = "Bandung"
 
-	fmt.Println(address1) // {Subang Jawa Barat Indonesia}
-	fmt.Println(address2) // {Bandung Jawa Barat Indonesia}
+	fmt.Println(address1) // {Bandung Jawa Barat Indonesia}
+	fmt.Println(address2) // &{Bandung Jawa Barat Indonesia}
+
 }
